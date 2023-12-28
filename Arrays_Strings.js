041d3253ec3ebba3e81ddfd35bdd813769cdce7b
Freshynex <bence.recsnik@gmail.com>
2023-12-28 11:38:16 +0100

    Update Arrays_Strings.js

--- Arrays_Strings.js
+++ Arrays_Strings.js
@@ -11,3 +11,31 @@ function camelize(str) {
 
       return words;
 }
+
+// returns new array with numbers between a and b
+  function filterRange(arr, a, b){
+    return arr.filter((e) => {
+      return (e >= a && e <= b)
+    })
+  }
+
+// mutates original array so it contains numbers between a and b
+
+function filterRangeInPlace(arr, a, b){
+
+  let indexesToRemove = [];
+	
+  for(let i=arr.length - 1; i>=0; i--){
+  	let element = arr[i];
+
+    if(!(element >= a && element <= b)){
+    	arr.splice(i, 1);
+    }
+  }
+}
+
+// sort array
+arr.sort((a, b) => a-b);
+
+//sort array in reverse
+arr.sort((a, b) => b-a);
